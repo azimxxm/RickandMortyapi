5b90b6120951040ef575985ad0312e82640ebddb
azimxxm <azimxxm@gmail.com>
2025-01-09 21:27:41 +0500

    Initial Commit

---
+++ RickandMortyapi/RickandMortyapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickandMortyapiApp.swift
+//  RickandMortyapi
+//
+//  Created by Azimjon Abdurasulov on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RickandMortyapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
